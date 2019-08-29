e5be0173110438e8d6fb5c082894053ca2027dfb
KRIS JEARAKUL <34535680+kj831ca@users.noreply.github.com>
2019-08-29 08:01:21 -0700

    Initial

---
+++ TXLaserModule/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCopyright("Copyright © Kevin KeYu Lei")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyProduct("TXLaserModule")]
+[assembly: AssemblyTitle("TXLaserModule")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: CompilationRelaxations(8)]
+[assembly: ComVisible(false)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
+[assembly: Guid("ef6a9f9d-2709-4b53-8e87-18553d22400c")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows=true)]
